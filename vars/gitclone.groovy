f27725d728e1451e0d0706695bf0357ee5f9a175
aachbehl <aachalbehl24@gmail.com>
2025-08-17 20:20:51 +0530

    Update gitclone.groovy

--- vars/gitclone.groovy
+++ vars/gitclone.groovy
@@ -1,5 +1,5 @@
 def call(String url,String branch){
   echo "This is copying the code"
-  git url:"${url}",branch:"${branch}
+  git url:"${url}",branch:"${branch}"
   echo "Code clones successfully"
 }
